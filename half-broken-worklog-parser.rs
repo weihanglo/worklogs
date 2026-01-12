2f73aae4621a355aafc7bf2197fe0fcb11f07a19
Weihang Lo <me@weihanglo.tw>
2026-01-12 01:44:02 -0500

    docs: remove unnecessary "Requirements" section

--- half-broken-worklog-parser.rs
+++ half-broken-worklog-parser.rs
@@ -6,12 +6,6 @@
 
 //! # A parser for my own half-broken worklog format
 //!
-//! ## Requirements
-//!
-//! - [Rust](https://rust-lang.org)
-//! - [xsv](https://github.com/BurntSushi/xsv)
-//! - [sort](https://en.wikipedia.org/wiki/Sort_(Unix))
-//!
 //! ## Worklog format
 //!
 //! The worklog may look like
@@ -20,21 +14,21 @@
 //! ## 2023-05-16
 //!
 //! * Issue triages
-//!     * Closed https://github.com/weihanglo/cargo/issues/12140
-//!     * Commented https://github.com/weihanglo/cargo/issues/12114
+//!   * Closed https://github.com/weihanglo/cargo/issues/12140
+//!   * Commented https://github.com/weihanglo/cargo/issues/12114
 //! * FCP reviews
-//!     * Merged https://github.com/weihanglo/cargo/pull/12146
+//!   * Merged https://github.com/weihanglo/cargo/pull/12146
 //! * PR submissions
-//!     * Created https://github.com/weihanglo/rust-analyzer/pull/14819
+//!   * Created https://github.com/weihanglo/rust-analyzer/pull/14819
 //!
 //! ## 2023-05-15
 //!
 //! * Issue triages
-//!     * Closed https://github.com/weihanglo/cargo/issues/2995
-//!     * Tracked https://github.com/weihanglo/cargo/issues/10490
-//!     * Commented https://github.com/weihanglo/cargo/issues/4184
+//!   * Closed https://github.com/weihanglo/cargo/issues/2995
+//!   * Tracked https://github.com/weihanglo/cargo/issues/10490
+//!   * Commented https://github.com/weihanglo/cargo/issues/4184
 //! * PR reviews
-//!     * Merged https://github.com/weihanglo/cargo/pull/12143
+//!   * Merged https://github.com/weihanglo/cargo/pull/12143
 //! ```
 //!
 //! ## Available categories
